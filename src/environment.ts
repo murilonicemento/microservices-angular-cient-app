3de71c80f84544c70f817e5ae8bbdb22ee22d3a4
murilonicemento <murilo_nascimento22@outlook.com>
2025-06-13 12:08:29 -0300

    feat: add microservices api url

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7243/api/auth/', // API base URL for development
+    apiUrl: 'https://localhost:7243/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'https://localhost:7121/api/products/'
   };
   
